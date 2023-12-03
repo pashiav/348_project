b89496c332c09092e23df8c6f30ee11a6d97a485
combs-a <combs.anya@gmail.com>
2023-12-03 17:56:53 -0600

    Added definitions in stack.h and created constructor/destructor for stack in stack.cpp.

--- program/include/stack.h
+++ program/include/stack.h
@@ -1,16 +1,27 @@
 #ifndef STACK_H
 #define STACK_H
 
+/*
+    Header file for the stack. Contains all the function definitions and variable declarations.
+    The stack is a data structure that uses an array to perform FILO based operations.
+*/
 
 class Stack
 {
     public:
-        Stack();
-        virtual ~Stack();
+        Stack();  // Constructor
+        ~Stack();  // Destructor
 
-    protected:
+        void push(char);  // Push to stack.
+        char pop();  // Pop from stack.
+        char peek();  // Peek at top of stack.
+        bool isEmpty();  // Check if the stack is empty.
 
     private:
+        char *arr;  // Pointer for the address of an array.
+        int top = -1;  // Keeps track of the top in the array. Default is set to -1, for an empty stack.
+        int stacksize = 99;  // How big the stack array is. The stack can hold up to 99 characters.
+
 };
 
 #endif // STACK_H
--- program/src/stack.cpp
+++ program/src/stack.cpp
@@ -1,11 +1,18 @@
 #include "stack.h"
 
+/*
+    Stack.cpp contains all of the functions for the stack.
+*/
+
+// Stack constructor. Creates the array for the stack to use.
 Stack::Stack()
 {
-    //ctor
+    char arr[stacksize];
 }
 
+
+// Stack destructor. Deletes the allocated memory.
 Stack::~Stack()
 {
-    //dtor
+    delete[] arr;
 }
