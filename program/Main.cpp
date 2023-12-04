85fac38e1900265f906bf0f6cbcc5ba9588a6443
mcauthon471 <114171555+mcauthon471@users.noreply.github.com>
2023-12-03 22:33:00 -0600

    Update Main.cpp

--- program/Main.cpp
+++ program/Main.cpp
@@ -9,24 +9,31 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    
+    // Introduction paragraph
+    cout << "Welcome to M.A.R.C.O.!\n"
+         << "M.A.R.C.O. is a terminal calculator that can handle simple maths.\n"
+         << "It can use +, -, *, \\, and % operators, as well as parentheses, and calculates using PEMDAS.\n"
+         << "For more detailed instructions, please look at the user manual in the docs folder.\n\n";
+
     string str;
 
-    // Prompt the user to enter an equation
-    cout << "Enter an equation to be solved: "; 
-    getline(cin, str);  // Read the entire line
+    while (1) { // While loop for continual use.
+        // Prompt the user to enter an equation
+        cout << "Enter an equation to be solved: ";
+        getline(cin, str);
 
-    // Create an instance of MyInfixCalculator
-    MyInfixCalculator infix_calculator;
-    
-    // Calculate the result of the infix equation
-    double result = infix_calculator.calculate(str);
+        // Create an instance of MyInfixCalculator
+        MyInfixCalculator infix_calculator;
 
-    // Set precision for displaying the result
-    std::cout.precision(3);
+        // Calculate the result of the infix equation
+        double result = infix_calculator.calculate(str);
 
-    // Display the calculated result
-    cout << std::fixed << "Result: " << result << endl;
+        // Set precision for displaying the result
+        cout.precision(3);
+
+        // Display the calculated result
+        cout << fixed << "Result: " << result << endl;
+    }
 
     return 0;
 }
